747d5ceec2ad1b5339c51bf0421b7379369203dd
Matt Ferrin <mattferrin@gmail.com>
2020-12-06 06:02:39 -0800

    fix unit tests on windows

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -12,18 +12,24 @@ struct ConfigFile {
     rules: Vec<String>,
 }
 
+/**
+ * Simplifies tests by displaying errors uniformly. Used cosmetically, not functionally.
+ */
+fn to_unix_string(name: String) -> String {
+    name.replace("\\", "/")
+}
+
 fn enforce_config(mut cx: FunctionContext) -> JsResult<JsString> {
     let config_file = match cx.argument_opt(0) {
         None => ".folderslintrc.json".to_owned(),
         Some(arg) => arg.downcast::<JsString>().or_throw(&mut cx)?.value(),
     };
-    let config_file_display = config_file.clone();
+    let config_file_display = to_unix_string(config_file.clone());
 
     let file = File::open(config_file).expect(&format!(
         "Could not open the config file `{}`",
         config_file_display
     ));
-    
     let reader = BufReader::new(file);
     let config_values: ConfigFile = serde_json::from_reader(reader).expect(&format!(
         "Could not parse the `{}` file",
@@ -57,11 +63,13 @@ fn enforce_config(mut cx: FunctionContext) -> JsResult<JsString> {
         if !satisfies_rule {
             panic!(format!(
                 "The following file path does not satisfy any rules `{}`",
-                entry_1.path().display()
+                to_unix_string(format!("{}", entry_1.path().display()))
             ));
         }
     }
     Ok(cx.string("No rule violations detected"))
 }
 
-register_module!(mut cx, { cx.export_function("enforceConfig", enforce_config) });
+register_module!(mut cx, {
+    cx.export_function("enforceConfig", enforce_config)
+});
